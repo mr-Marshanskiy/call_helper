3c7461cfa1e72070bcc655100aa6fbe5a37a944c
n.marshanskiy <n.marshanskiy@altezza.board>
2023-01-25 22:56:15 +0300

    fix: email on creating

--- users/models/managers.py
+++ users/models/managers.py
@@ -14,7 +14,7 @@ class CustomUserManager(BaseUserManager):
 
         if not username:
             if email:
-                username = email.split('@')[0]
+                username = email
             else:
                 username = phone_number
 

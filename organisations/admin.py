c922a7ecc31d6b46346fd80dd25c2ea9f4b2b72e
n.marshanskiy <n.marshanskiy@altezza.board>
2023-01-30 20:22:24 +0300

    refactor: Move organisations and groups to new app

---
+++ organisations/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+
+from breaks.models.replacements import GroupInfo
+from organisations.models import organisations, groups, dicts
+
+
+##############################
+# INLINES
+##############################
+class EmployeeInline(admin.TabularInline):
+    model = organisations.Employee
+    fields = ('user', 'position', 'date_joined',)
+
+
+class MemberInline(admin.TabularInline):
+    model = groups.Member
+    fields = ('user', 'date_joined',)
+
+
+class ProfileBreakInline(admin.StackedInline):
+    model = GroupInfo
+    fields = (
+        'min_active',
+        'break_start',
+        'break_end',
+        'break_max_duration',
+    )
+
+
+##############################
+# MODELS
+##############################
+@admin.register(dicts.Position)
+class ReplacementStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'code', 'name', 'sort', 'is_active',
+    )
+
+
+@admin.register(organisations.Organisation)
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'director',)
+    filter_vertical = ('employees',)
+    inlines = (EmployeeInline,)
+
+
+@admin.register(groups.Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'manager', 'min_active',)
+    list_display_links = ('id', 'name',)
+    search_fields = ('name',)
+    inlines = (
+        MemberInline,
+        ProfileBreakInline,
+    )

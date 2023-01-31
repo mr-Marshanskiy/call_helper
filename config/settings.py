50d4cc8299132e58a903b94cced068b847328b6f
n.marshanskiy <n.marshanskiy@altezza.board>
2023-01-31 19:01:08 +0300

    fix: activate JWT auth

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',),
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
 

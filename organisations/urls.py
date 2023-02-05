94b8971bb8691bf66ad63bc4d7ed15fcd6106184
n.marshanskiy <n.marshanskiy@altezza.board>
2023-02-05 20:23:33 +0300

    fix: groups

--- organisations/urls.py
+++ organisations/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from organisations.views import dicts, organisations, employees
+from organisations.views import dicts, organisations, employees, groups
 
 router = DefaultRouter()
 
 router.register(r'dicts/positions', dicts.PositionView, 'positions')
 router.register(r'search', organisations.OrganisationSearchView, 'organisations-search')
 router.register(r'(?P<pk>\d+)/employees', employees.EmployeeView, 'employees')
+router.register(r'groups', groups.GroupView, 'groups')
 router.register(r'', organisations.OrganisationView, 'organisations')
 
 urlpatterns = [

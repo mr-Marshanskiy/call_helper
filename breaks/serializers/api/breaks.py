0c7de4b1d0315b582936e4ee2e4ed2b543a82fc8
n.marshanskiy <n.marshanskiy@altezza.board>
2023-03-26 16:01:40 +0300

    fix: status fix

--- breaks/serializers/api/breaks.py
+++ breaks/serializers/api/breaks.py
@@ -185,7 +185,8 @@ class BreakScheduleSerializer(serializers.Serializer):
         break_start = instance.break_start.strftime('%H:%M')
         break_end = instance.break_end.strftime('%H:%M')
         value = f'{break_start} - {break_end}'
-        return self._convert_to_cell(value, span=span)
+        color = '#78ffae'
+        return self._convert_to_cell(value, color, span)
 
     def get_post_blank(self, instance):
         span = self._get_span_count(instance.break_end, instance.replacement.break_end)
